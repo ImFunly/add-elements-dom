e62eeea81e0cd501ffbaec51898cb65febaae4dc
Samuel Pérez <samux7421x@hotmail.com>
2024-02-23 15:17:57 +0100

    Ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
- // Aquí tu código
+// Aquí tu código
+const lista = document.getElementById("lista");
+const agregar = document.getElementById("agregar");
+
+function añadirElemento() {
+  let elemento = prompt("Añade un elemento a la lista");
+
+  let listar = document.createElement("li");
+  listar.textContent = elemento;
+  lista.appendChild(listar);
+}
+
+agregar.addEventListener("click", añadirElemento);
